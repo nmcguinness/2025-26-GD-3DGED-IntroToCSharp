ec0d773c2b0a3730ec5cc5c8588832bc9f3c462b
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-09-23 12:10:54 +0100

    Adding Player

--- IntroToCSharp/Exercises/Player.cs
+++ IntroToCSharp/Exercises/Player.cs
@@ -1,11 +1,32 @@
 ﻿
+using System.Numerics;
+
 namespace Exercises
 {
     public class Player
     {
         //Fields
+        private string name;
+        private int health;
+        private Vector3 position;
+
+        public string Name { get => name; set => name = value; }
+        public int Health { get => health; set => health = value; }
+        public Vector3 Position { get => position; set => position = value; }
+
 
         //Constructors
+        public Player(string name, int health)
+            : this(name, health, Vector3.Zero)
+        {
+           
+        }
+        public Player(string name, int health, Vector3 position)
+        {
+            this.name = name;
+            this.health = health;
+            this.position = position;
+        }
 
         //ToString
     }
